ff20888a4c371be18a5a0915352875b9b07a3fe5
hjJoo <dodam.janna1997@gmail.com>
2017-12-11 03:43:23 +0900

    Revert "avoidBomb"
    This reverts commit 750706bce42369ad0045fe2bef23606fbf641865.

--- javaTermProject/avoidBomb/src/lowLevel/Main.java
+++ javaTermProject/avoidBomb/src/lowLevel/Main.java
@@ -1,9 +1,12 @@
-package bombGame;
+package lowLevel;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		// board 蔼 积己
+		new GameSystem();
+		
 		// 霸烙 Paly Frame 积己
 		new MyFrame();
 		
