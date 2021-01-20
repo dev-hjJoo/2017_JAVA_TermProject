f77683e8c799daef9a3693011363c1150d5f9260
ki-jung-kyun <76971089+ki-jung-kyun@users.noreply.github.com>
2021-01-20 22:09:20 +0900

    change design2

--- JAVA_FirstGame/src/main/Main.java
+++ JAVA_FirstGame/src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.Color;
 import java.io.IOException;
 
 import javax.sound.sampled.LineUnavailableException;
@@ -11,7 +12,7 @@ public class Main {
 	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 		MainFrame frame = new MainFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(450,500);
+		frame.setSize(515,700);
 		frame.setVisible(true);
 		
 		
--- JAVA_FirstGame/src/nyangIGame/GameRules.java
+++ JAVA_FirstGame/src/nyangIGame/GameRules.java
@@ -12,7 +12,7 @@ public class GameRules extends JFrame {
 	public GameRules() {
 		setTitle("게임방법");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		icon = new ImageIcon("");
+		icon = new ImageIcon("images/배경.png");
 		JPanel background = new JPanel() {
 			public void paintComponent(Graphics g) {
 				g.drawImage(icon.getImage(), 0,0,null);
@@ -28,21 +28,21 @@ public class GameRules extends JFrame {
 	
 		
 		//game information layout
-		JLabel text1 = new JLabel("게임 목표!!");
+		JLabel text1 = new JLabel("게임 목표");
 		text1.setLocation(150,-30);
 		text1.setSize(250,100);
-		text1.setFont(new Font("궁서", Font.BOLD, 30));
+		text1.setFont(new Font("바탕", Font.BOLD, 30));
 		background.add(text1);
 		
 		JLabel text2 = new JLabel("시간 내에 두더냥을 잡아 점수를 얻어");
 		text2.setLocation(90,30);
 		text2.setSize(300,100);
-		text2.setFont(new Font("궁서", Font.ITALIC, 17));
+		text2.setFont(new Font("바탕", Font.BOLD, 17));
 		background.add(text2);
 		JLabel text3 = new JLabel("친구들과 경쟁하여 승리해보자!!");
 		text3.setLocation(105,60);
 		text3.setSize(250,100);
-		text3.setFont(new Font("궁서", Font.ITALIC, 17));
+		text3.setFont(new Font("바탕", Font.BOLD, 17));
 		background.add(text3);
 		
 		
@@ -50,20 +50,20 @@ public class GameRules extends JFrame {
 		JLabel mani = new JLabel("게임 설명");
 		mani.setLocation(150,120);
 		mani.setSize(250,100);
-		mani.setFont(new Font("궁서", Font.BOLD, 30));
+		mani.setFont(new Font("바탕", Font.BOLD, 30));
 		background.add(mani);
 		
 		
 		JLabel mani1 = new JLabel("키보드 방향키를 이용해 조준할 수 있습니다.");
 		mani1.setLocation(20,150);
 		mani1.setSize(430,200);
-		mani1.setFont(new Font("궁서", Font.BOLD, 20));
+		mani1.setFont(new Font("바탕", Font.BOLD, 20));
 		background.add(mani1);
 		
 		JLabel mani2 = new JLabel("스페이스바를 눌러 잡을 수 있습니다.");
 		mani2.setLocation(50,200);
 		mani2.setSize(370,200);
-		mani2.setFont(new Font("궁서", Font.BOLD, 20));
+		mani2.setFont(new Font("바탕", Font.BOLD, 20));
 		background.add(mani2);
 		
 
@@ -80,7 +80,7 @@ public class GameRules extends JFrame {
 	JLabel textcat = new JLabel("고양이를 잡으면 1점 획득");
 	textcat.setLocation(150,380);
 	textcat.setSize(250,100);
-	textcat.setFont(new Font("궁서", Font.BOLD, 17));
+	textcat.setFont(new Font("바탕", Font.BOLD, 17));
 	background.add(textcat);
 	
 
@@ -95,7 +95,7 @@ public class GameRules extends JFrame {
 	JLabel textdog = new JLabel("강아지를 잡으면 -3점 손실");
 	textdog.setLocation(150,450);
 	textdog.setSize(250,100);
-	textdog.setFont(new Font("궁서", Font.BOLD, 17));
+	textdog.setFont(new Font("바탕", Font.BOLD, 17));
 	background.add(textdog);
 	
 	//fish image layout
@@ -110,18 +110,10 @@ public class GameRules extends JFrame {
 	textfish.setLocation(150,520);
 	textfish.setSize
 	(250,100);
-	textfish.setFont(new Font("궁서", Font.BOLD, 17));
+	textfish.setFont(new Font("바탕", Font.BOLD, 17));
 	background.add(textfish);
 	
 	
-//	//keyboard image layout
-//	
-//	ImageIcon keyboard1 = new ImageIcon("images/klipartz.png");
-//	JLabel keyboard3 = new JLabel(keyboard1);
-//	
-//	keyboard3.setLocation(20,250);
-//	keyboard3.setSize(100,100);
-//	background.add(keyboard3);
 	}
 	public static void main(String[] args) {
 		GameRules frame = new GameRules();
